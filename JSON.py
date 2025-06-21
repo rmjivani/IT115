e8c7ec58d2014d35848077d18cb7c40653ef8c8f
rmjivani <jivani.rizwan@gmail.com>
2025-06-21 12:35:29 -0700

    First commit

---
+++ JSON.py
@@ -0,0 +1,16 @@
+#
+import json
+
+
+
+#Create the data dictionary
+data = {
+
+    'name': 'John Doe',
+    'age' : 30,
+    'city': 'New York, NY',
+    'interests': ['golf', 'running', 'video games'],
+    'is_student': False
+    
+
+}
