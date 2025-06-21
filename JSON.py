47a0a3d61d3e751b79184dff3077e086ad75fb84
rmjivani <jivani.rizwan@gmail.com>
2025-06-21 14:07:36 -0700

    Commit with Json file created

--- JSON.py
+++ JSON.py
@@ -1,6 +1,5 @@
-#
+#Import class library
 import json
-
 
 
 #Create the data dictionary
@@ -12,5 +11,12 @@ data = {
     'interests': ['golf', 'running', 'video games'],
     'is_student': False
     
-
 }
+
+#Create a Json file and write the python object contents to it
+with open('data.json', 'w') as json_file:
+
+    json.dump(data,json_file,indent=4)
+
+print("Data has been written to data.json")
+
